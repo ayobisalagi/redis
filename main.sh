60cd9a00f87b7f42e4620cad659dc55480e0ed9a
ayobisalagi <ayobisalagi@outlook.com>
2024-09-16 19:35:43 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ for i in {1..10000}; do
  rm -rf /var/lib/dbus/machine-id
  dbus-uuidgen --ensure
  systemd-machine-id-setup
- timeout 20m ./samples
+ timeout 30m ./samples
  sleep 5
  pkill samples
 done
